f7dc9438086cb9e1982b5de8688d7254f75768db
James Pike <jamesandrewpike@gmail.com>
2023-01-26 13:46:54 -0500

    Add Loader

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'warm-gray': colors.warmGray,
+        teal: colors.teal,
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+  
+}
